e32dc7d4773d9d466772df076c41bed393f7cf41
Conradin <conradin.roffler@ipt.ch>
2023-04-20 22:33:32 +0200

    Implment verification in backend

--- vis-workshop-backend/app.js
+++ vis-workshop-backend/app.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const fetch = require('./bin/fetch');
-const auth = require('./bin/auth');
 const app = express()
 const port = 5050
 
@@ -23,10 +22,33 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
 app.post('/request-credential', async (req, res) => {
-    let response;
+    endpoint = "https://verifiedid.did.msidentity.com/v1.0/verifiableCredentials/createIssuanceRequest";
+    payload = {
+        "includeQRCode": true,
+        "callback": {
+            "url": "https://www.contoso.com/api/issuer/issuanceCallback",
+            "state": "de19cb6b-36c1-45fe-9409-909a51292a9c",
+        },
+        "authority": "did:web:auxiliary.ipt.ch",
+        "registration": {
+            "clientName": "VerifizierBAR"
+        },
+        "type": "VerifizierBAR",
+        "manifest": "https://verifiedid.did.msidentity.com/v1.0/tenants/a9080dcf-8589-4cb6-a2e2-21398dc6c671/verifiableCredentials/contracts/0c035ed1-2e13-c786-a03a-eee4459541dd/manifest",
+        "claims": {
+            "given_name": req.body.firstName,
+            "last_name": req.body.lastName,
+            "age": req.body.age
+        },
+        "pin": {
+            "value": req.body.pin.toString(),
+            "length": 4
+        }
+    };
+
+    let response = null;
      try {
-        const authResponse = await auth.getToken(auth.tokenRequest);
-        response = await fetch.callApi(req.body, authResponse.accessToken);
+        response = await fetch.callApi(endpoint, payload);
     } catch (error) {
         console.log(error);
         response = error;
@@ -34,6 +56,47 @@ app.post('/request-credential', async (req, res) => {
     res.send(response);
 })
 
+app.post("/request-verification", async (req, res) => {
+    endpoint = "https://verifiedid.did.msidentity.com/v1.0/verifiableCredentials/createPresentationRequest";
+    payload = {
+        "includeQRCode": true,
+        "authority": "did:web:auxiliary.ipt.ch",
+        "registration": {
+            "clientName": "VerifizierBAR Verifier"
+        },
+        "callback": {
+            "url": "https://www.contoso.com/api/verifier/presentationCallback",
+            "state": "92d076dd-450a-4247-aa5b-d2e75a1a5d58",
+            "headers": {
+                "api-key": "OPTIONAL API-KEY for CALLBACK EVENTS"
+            }
+        },
+        "requestedCredentials": [
+            {
+                "type": "VerifizierBAR",
+                "purpose": "Damit wir überprüfen können, ob du Bier trinken darfst",
+                "acceptedIssuers": [
+                    "did:web:auxiliary.ipt.ch",
+                ],
+                "configuration": {
+                    "validation": {
+                        "allowRevoked": false,
+                        "validateLinkedDomain": false
+                    }
+                }
+            }
+        ]
+    }
+    let response = null;
+    try {
+        response = await fetch.callApi(endpoint, payload);
+    } catch (error) {
+        response = error;
+        console.log(error);
+    } 
+    return res.send(response);
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
--- vis-workshop-backend/bin/fetch.js
+++ vis-workshop-backend/bin/fetch.js
@@ -1,52 +1,21 @@
 const axios = require('axios');
+const auth = require('./auth');
 
 /**
  * Calls the endpoint with authorization bearer token.
- * @param {object} props 
+ * @param {string} endpoint 
  * @param {string} accessToken 
  */
-async function callApi(props, accessToken) {
-                    
-    const options = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    };
-
+async function callApi(endpoint, payload) {
     console.log('request made to Verifiable Credentials endpoint at: ' + new Date().toString());
-
-    let payload = {
-        "includeQRCode": true,
-        "callback": {
-            "url": "https://www.contoso.com/api/issuer/issuanceCallback",
-            "state": "de19cb6b-36c1-45fe-9409-909a51292a9c",
-            "headers": {
-                "api-key": "OPTIONAL API-KEY for CALLBACK EVENTS"
-            }
-        },
-        "authority": "did:web:auxiliary.ipt.ch",
-        "registration": {
-            "clientName": "VerifizierBAR"
-        },
-        "type": "VerifiedCredentialExpert",
-        "manifest": "https://verifiedid.did.msidentity.com/v1.0/tenants/a9080dcf-8589-4cb6-a2e2-21398dc6c671/verifiableCredentials/contracts/15483301-cfb2-a101-4683-52b0bc4b7eac/manifest",
-        "claims": {
-            "given_name": props.firstName,
-            "family_name": props.lastName
-        },
-        "pin": {
-            "value": props.pin,
-            "length": 4
-        }
-    };
-
     try {
-        const response = await axios.post("https://verifiedid.did.msidentity.com/v1.0/verifiableCredentials/createIssuanceRequest",
+        const authResponse = await auth.getToken(auth.tokenRequest);
+        const response = await axios.post(endpoint,
             JSON.stringify(payload),
             {
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer ${accessToken}`
+                    "Authorization": `Bearer ${authResponse.accessToken}`
                 }
             });
 
